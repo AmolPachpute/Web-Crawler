f9487e871de6948a7adf7e8588565783d56de83c
AmolPachpute <amolpachpute94@gmail.com>
2015-07-02 18:11:31 +0530

    updated spider

--- Docomo_crawler/Docomo_crawler/spiders/docomo_spider.py
+++ Docomo_crawler/Docomo_crawler/spiders/docomo_spider.py
@@ -1,6 +1,8 @@
 import scrapy
 import ipdb
 #from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+import csv
 class DocomoSpider(scrapy.Spider):
     name = "docomo_plans"
     allowed_domains = ["plansinfo.com"]
@@ -9,11 +11,27 @@ class DocomoSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        ipdb.set_trace()
+
         filename = response.url.split("/")[-2] + '.html'
         plans_list = [str(i) for i in response.selector.xpath("//h3/text()").extract()[:6]]
         titles_list = [str(i) for i in response.selector.xpath("//table/tr/th/b/text()").extract()[0:4]]
+        file_obj = open(str(plans_list[0] + ".csv"), "wb")
 
 
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        writer_obj = csv.writer(file_obj, delimiter=",")
+        writer_obj.writerow([plans_list[0]])
+        writer_obj.writerow(titles_list)
+
+
+        ipdb.set_trace()
+        for table_row in response.selector.xpath("//table[1]//tr").extract():
+            row_response_object = HtmlResponse(url="", body=str(table_row))
+            list1 = [str(i) for i in row_response_object.selector.xpath("//td/text()").extract()]
+            writer_obj.writerow(list1)
+        file_obj.close()
+
+
+
+
+
+
